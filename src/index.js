f9640cf03e3d8599764c84b24e086bff1af49451
shagufta <shagufta.batool8@gmail.com>
2021-12-02 14:01:39 +0100

    my first react app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import React from "react-dom";
+//import Hello from "./Hello";
+
+const HelloWorld = () => {
+
+    return <h2>hello world</h2>;
+};
+
+ReactDOM.render(<HelloWorld/>, document.getElementById("app"));
